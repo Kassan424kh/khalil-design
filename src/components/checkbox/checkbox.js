1e3b566d05f713fa4e029de386854cfa38fa418a
Khalil Khalil <46401401+Kassan424kh@users.noreply.github.com>
2022-10-18 08:20:57 +0200

    [Checkbox]: added indeterminate

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,49 +1,42 @@
-import React, { useState } from "react";
-import "./styles.sass";
+import React, { useState } from 'react'
+import './styles.sass'
 
 const Checkbox = ({
-  checked,
-  className,
-  primary,
-  disabled,
-  children,
-  onCheck,
-  left,
-  right
+    checked,
+    indeterminate = false,
+    className,
+    primary,
+    disabled,
+    children,
+    onCheck,
+    left,
+    right,
+    ...props
 }) => {
-  const [_checked, _isChecked] = useState(checked ?? false);
+    const [_checked, _isChecked] = useState(checked ?? false)
 
-  return (
-    <div
-      className={`checkbox disable-selecting ${className ?? ""}${
-        primary ? " primary" : ""
-      }${disabled ? " disabled" : ""}`}
-      onClick={() => {
-        if (onCheck) onCheck(!checked);
-        _isChecked(!_checked);
-      }}
-    >
-      {right && !left ? (
-        <div className={"children right"}>{children}</div>
-      ) : null}
-      <span
-        className={`checkbox-icon${
-          checked !== undefined
-            ? checked && !disabled
-              ? " checked"
-              : ""
-            : _checked && !disabled
-            ? " checked"
-            : ""
-        }`}
-      >
-        <span className={"material-icons-outlined"}>check</span>
-      </span>
-      {left || (!left && !right) ? (
-        <div className={"children left"}>{children}</div>
-      ) : null}
-    </div>
-  );
-};
+    return (
+        <div
+            {...props}
+            className={`checkbox disable-selecting ${className ?? ''}${primary ? ' primary' : ''}${
+                disabled ? ' disabled' : ''
+            }`}
+            onClick={() => {
+                if (onCheck) onCheck(!checked)
+                _isChecked(!_checked)
+            }}
+        >
+            {right && !left ? <div className={'children right'}>{children}</div> : null}
+            <span
+                className={`checkbox-icon${
+                    checked ? (!disabled ? ' checked' : '') : _checked && !disabled ? ' checked' : ''
+                } ${indeterminate ? ' indeterminate' : ''}`}
+            >
+                <span className={'material-icons-outlined'}>{indeterminate ? 'remove' : 'check'}</span>
+            </span>
+            {left || (!left && !right) ? <div className={'children left'}>{children}</div> : null}
+        </div>
+    )
+}
 
-export default Checkbox;
+export default Checkbox
